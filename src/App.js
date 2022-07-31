cb6c2e89d69db8fc8f8bb401ece3bccf4f00f978
leeeha <jxlhe46@seoultech.ac.kr>
2022-07-31 13:15:34 +0900

    only react

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import AddNumberRoot from "./components/AddNumberRoot";
 import DisplayNumberRoot from "./components/DisplayNumberRoot";
 import './App.css';
 
-class App extends Component() {
+class App extends Component {
+  state = {
+    number: 0 
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Root</h1>
-        <AddNumberRoot></AddNumberRoot>
-        <DisplayNumberRoot></DisplayNumberRoot>
+
+        <AddNumberRoot onClick={function(size){
+          this.setState({number: this.state.number + size}); 
+        }.bind(this)}></AddNumberRoot>
+
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
       </div>
     );
   }
